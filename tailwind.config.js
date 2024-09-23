66ecfa927ecce2f5530ba91aef6ef13b41df3420
ArmaVargas <armando091994@gmail.com>
2024-09-23 17:07:11 -0500

    dando estilos al desktop

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "PaleBlue": "hsl(225, 100%, 94%)",
+        "BrightBlue": "hsl(245, 75%, 52%)", // color de boton
+        "VeryPaleBlue": "hsl(225, 100%, 98%)",
+        "DesaturatedBlue": "hsl(224, 23%, 55%)",
+        "DarkBlue": "hsl(223, 47%, 23%)"
+      },
+
+      fontFamily:{
+        "RedHad": ["Red Hat Display", "sans-serif"]
+      },
+
+      backgroundImage:{
+        'iconMusic':"url('./images/icon-music.svg')",
+        'ilustracion': "url('./images/illustration-hero.svg')",
+        'Bdestokp': "url('./images/pattern-background-desktop.svg')",
+        'Bmobile': "url('./images/pattern-background-mobile.svg')"
+        
+      }
+  },
+  plugins: [],
+}
